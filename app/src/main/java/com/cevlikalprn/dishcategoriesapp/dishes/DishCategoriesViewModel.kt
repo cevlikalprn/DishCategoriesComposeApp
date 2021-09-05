b8157413fd6961e50f0bead54a389f8cab59d206
Alperen <devcalprn@gmail.com>
2021-09-05 11:56:01 +0300

    DishCategoriesViewModel.kt updated

--- app/src/main/java/com/cevlikalprn/dishcategoriesapp/dishes/DishCategoriesViewModel.kt
+++ app/src/main/java/com/cevlikalprn/dishcategoriesapp/dishes/DishCategoriesViewModel.kt
@@ -1,6 +1,26 @@
 package com.cevlikalprn.dishcategoriesapp.dishes
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.cevlikalprn.dishcategoriesapp.model.Dish
+import com.cevlikalprn.dishcategoriesapp.repository.DishesRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class DishCategoriesViewModel: ViewModel() {
+class DishCategoriesViewModel(private val repository: DishesRepository = DishesRepository()): ViewModel() {
+
+    val dishesState: MutableState<List<Dish>> = mutableStateOf(emptyList<Dish>())
+
+    init {
+        viewModelScope.launch(Dispatchers.IO){
+            dishesState.value = getDishes()
+        }
+    }
+
+    private suspend fun getDishes(): List<Dish>{
+        return repository.getDishCategories().dishCategories
+    }
+
 }
